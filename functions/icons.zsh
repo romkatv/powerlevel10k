fa21bfff4595eb9451917ed219d29b83ea74c2e4
rjorgenson <rjorgenson@gmail.com>
2016-12-30 14:10:43 -0700

    reverted trailing space edit

--- functions/icons.zsh
+++ functions/icons.zsh
@@ -132,7 +132,7 @@ case $POWERLEVEL9K_MODE in
       VCS_GIT_GITLAB_ICON            $'\uF296 '             # 
       VCS_HG_ICON                    $'\uF0C3 '             # 
       VCS_SVN_ICON                   '(svn) '
-      RUST_ICON                      $'\uE6A8'              # 
+      RUST_ICON                      $'\uE6A8'              #  
       PYTHON_ICON                    $'\U1F40D'             # 🐍
       SWIFT_ICON                     ''
       PUBLIC_IP_ICON                 ''
