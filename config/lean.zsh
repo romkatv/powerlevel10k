b51e263637dd1102f6a7d4e366807c06825383e2
romkatv <roman.perepelitsa@gmail.com>
2019-07-12 15:47:13 +0200

    draft of lean config

---
+++ config/lean.zsh
@@ -0,0 +1,202 @@
+if [[ -o 'aliases' ]]; then
+  'builtin' 'unsetopt' 'aliases'
+  local p9k_lean_restore_aliases=1
+else
+  local p9k_lean_restore_aliases=0
+fi
+
+() {
+  emulate -L zsh
+  setopt no_unset
+
+  typeset -ga POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(
+      # Line #1
+      dir          # current directory
+      vcs          # git status
+      # Line #2
+      newline
+      prompt_char  # prompt symbol
+  )
+
+  typeset -ga POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(
+      status                  # exit code of the last command
+      command_execution_time  # duration of the last command
+      background_jobs         # presence of background jobs
+      # virtualenv            # python virtual environment (https://docs.python.org/3/library/venv.html)
+      # anaconda              # conda environment (https://conda.io/)
+      # pyenv                 # python environment (https://github.com/pyenv/pyenv)
+      # kubecontext           # current kubernetes context (https://kubernetes.io/)
+      context                 # user@host
+      # time                  # current time
+  )
+
+  # No background colors.
+  typeset -g POWERLEVEL9K_BACKGROUND=
+  # No segment icons.
+  typeset -g POWERLEVEL9K_VISUAL_IDENTIFIER_EXPANSION=
+  # No whitespace within segments.
+  typeset -g POWERLEVEL9K_{LEFT,RIGHT}_{LEFT,MIDDLE,RIGHT}_WHITESPACE=
+  # No trailing space.
+  typeset -g POWERLEVEL9K_LEFT_SEGMENT_END_SEPARATOR=
+  # Don't push right prompt to the last prompt line.
+  typeset -g POWERLEVEL9K_RPROMPT_ON_NEWLINE=false
+  # Separate segments with a space.
+  typeset -g POWERLEVEL9K_{LEFT,RIGHT}_SUBSEGMENT_SEPARATOR=' '
+  typeset -g POWERLEVEL9K_{LEFT,RIGHT}_SEGMENT_SEPARATOR=
+
+  # Don't add an empty line before each prompt. If you set it to true, you'll probably
+  # want to set POWERLEVEL9K_SHOW_RULER=false below.
+  typeset -g POWERLEVEL9K_PROMPT_ADD_NEWLINE=false
+
+  # Add a horizontal line before each prompt. If you set it to false, you might want to
+  # set POWERLEVEL9K_PROMPT_ADD_NEWLINE=true above.
+  typeset -g POWERLEVEL9K_SHOW_RULER=true
+  typeset -g POWERLEVEL9K_RULER_CHAR='─'
+  typeset -g POWERLEVEL9K_RULER_FOREGROUND=237
+
+  # Green prompt symbol on success ($? == 0).
+  typeset -g POWERLEVEL9K_PROMPT_CHAR_OK_{VIINS,VICMD,VIVIS}_FOREGROUND=76
+  # Red prompt symbol on error ($? != 0).
+  typeset -g POWERLEVEL9K_PROMPT_CHAR_ERROR_{VIINS,VICMD,VIVIS}_FOREGROUND=196
+  # Default prompt symbol.
+  typeset -g POWERLEVEL9K_PROMPT_CHAR_{OK,ERROR}_VIINS_CONTENT_EXPANSION='❯ '
+  # Prompt symbol in command vi mode.
+  typeset -g POWERLEVEL9K_PROMPT_CHAR_{OK,ERROR}_VICMD_CONTENT_EXPANSION='❮ '
+  # Prompt symbol in visual vi mode.
+  typeset -g POWERLEVEL9K_PROMPT_CHAR_{OK,ERROR}_VIVIS_CONTENT_EXPANSION='Ⅴ '
+
+  # Default current directory color.
+  typeset -g POWERLEVEL9K_DIR_FOREGROUND=39
+  # Directory color if it isn't writable.
+  typeset -g POWERLEVEL9K_DIR_NOT_WRITABLE_FOREGROUND=209
+  # Embed a hyperlink into the directory.
+  typeset -g POWERLEVEL9K_DIR_HYPERLINK=true
+  # Enable special styling for non-writable directories. If set to false,
+  # POWERLEVEL9K_DIR_NOT_WRITABLE_FOREGROUND defined above won't have effect.
+  typeset -g POWERLEVEL9K_DIR_SHOW_WRITABLE=true
+
+  # Git status: feature:master#tag ⇣42⇡42 *42 merge ~42 +42 !42 ?42
+  local vcs=''
+  # 'feature' or '@72f5c8a' if not on a branch
+  vcs+='%76F${${VCS_STATUS_LOCAL_BRANCH//\%/%%}:-%f@%76F${VCS_STATUS_COMMIT[1,8]}}'
+  # ':master' if the tracking branch name differs from local branch
+  vcs+='${${VCS_STATUS_REMOTE_BRANCH:#$VCS_STATUS_LOCAL_BRANCH}:+%f:%76F${VCS_STATUS_REMOTE_BRANCH//\%/%%}}'
+  # '#tag' if on a tag
+  vcs+='${VCS_STATUS_TAG:+%f#%76F${VCS_STATUS_TAG//\%/%%}}'
+  # ⇣42 if behind the remote
+  vcs+='${${VCS_STATUS_COMMITS_BEHIND:#0}:+ %76F⇣${VCS_STATUS_COMMITS_BEHIND}}'
+  # ⇡42 if ahead of the remote; no leading space if also behind the remote: ⇣42⇡42
+  vcs+='${${VCS_STATUS_COMMITS_AHEAD:#0}:+${${(M)VCS_STATUS_COMMITS_BEHIND:#0}:+ }%76F⇡${VCS_STATUS_COMMITS_AHEAD}}'
+  # *42 if have stashes
+  vcs+='${${VCS_STATUS_STASHES:#0}:+ %76F*${VCS_STATUS_STASHES}}'
+  # 'merge' if the repo is in an unusual state
+  vcs+='${VCS_STATUS_ACTION:+ %196F${VCS_STATUS_ACTION//\%/%%}}'
+  # ~42 if have merge conflicts
+  vcs+='${${VCS_STATUS_NUM_CONFLICTED:#0}:+ %196F~${VCS_STATUS_NUM_CONFLICTED}}'
+  # +42 if have staged changes
+  vcs+='${${VCS_STATUS_NUM_STAGED:#0}:+ %11F+${VCS_STATUS_NUM_STAGED}}'
+  # !42 if have unstaged changes
+  vcs+='${${VCS_STATUS_NUM_UNSTAGED:#0}:+ %11F!${VCS_STATUS_NUM_UNSTAGED}}'
+  # ?42 if have untracked files
+  vcs+='${${VCS_STATUS_NUM_UNTRACKED:#0}:+ %12F?${VCS_STATUS_NUM_UNTRACKED}}'
+  # If P9K_CONTENT is not empty, leave it unchanged. It's either "loading" or from vcs_info.
+  vcs="\${P9K_CONTENT:-$vcs}"
+
+  # Disable the default Git status formatting.
+  typeset -g POWERLEVEL9K_VCS_DISABLE_GITSTATUS_FORMATTING=true
+  # Install our own Git status formatting.
+  typeset -g POWERLEVEL9K_VCS_{CLEAN,UNTRACKED,MODIFIED}_CONTENT_EXPANSION=$vcs
+  # When Git status is being refreshed asynchronously, display the last known repo status in grey.
+  typeset -g POWERLEVEL9K_VCS_LOADING_CONTENT_EXPANSION=${${vcs//\%f}//\%<->F}
+  typeset -g POWERLEVEL9K_VCS_LOADING_FOREGROUND=244
+  # Enable counters for staged, unstaged, etc.
+  typeset -g POWERLEVEL9K_VCS_{STAGED,UNSTAGED,UNTRACKED,COMMITS_AHEAD,COMMITS_BEHIND}_MAX_NUM=-1
+
+  # These settings have no effect when vcs prompt is rendered from gitstatusd data.
+  # They are used for respositories other than Git or when gitstatusd fails and Powerlevel10k
+  # has to fall back to using vcs_info.
+  typeset -g POWERLEVEL9K_VCS_CLEAN_FOREGROUND=76
+  typeset -g POWERLEVEL9K_VCS_MODIFIED_FOREGROUND=11
+  typeset -g POWERLEVEL9K_VCS_UNTRACKED_FOREGROUND=76
+  typeset -g POWERLEVEL9K_VCS_REMOTE_BRANCH_ICON=':'
+  typeset -g POWERLEVEL9K_VCS_COMMIT_ICON='@'
+  typeset -g POWERLEVEL9K_VCS_INCOMING_CHANGES_ICON='⇣'
+  typeset -g POWERLEVEL9K_VCS_OUTGOING_CHANGES_ICON='⇡'
+  typeset -g POWERLEVEL9K_VCS_STASH_ICON='*'
+  typeset -g POWERLEVEL9K_VCS_TAG_ICON=$'%{\b#%}'
+  typeset -g POWERLEVEL9K_VCS_UNTRACKED_ICON=$'%{\b?%}'
+  typeset -g POWERLEVEL9K_VCS_UNSTAGED_ICON=$'%{\b!%}'
+  typeset -g POWERLEVEL9K_VCS_STAGED_ICON=$'%{\b+%}'
+  typeset -g POWERLEVEL9K_VCS_BRANCH_ICON=
+
+  # Don't show status on success.
+  typeset -g POWERLEVEL9K_STATUS_OK=false
+  # Error status color.
+  typeset -g POWERLEVEL9K_STATUS_ERROR_FOREGROUND=9
+  # Don't show status unless the last command was terminated by a signal.
+  # Show the signal as INT, ABORT, KILL, etc.
+  typeset -g POWERLEVEL9K_STATUS_ERROR_CONTENT_EXPANSION='${${P9K_CONTENT#SIG}//[!A-Z]}'
+
+  # Show execution time of the last command if it's longer than this many seconds.
+  typeset -g POWERLEVEL9K_COMMAND_EXECUTION_TIME_THRESHOLD=0
+  # Execution time color.
+  typeset -g POWERLEVEL9K_COMMAND_EXECUTION_TIME_FOREGROUND=101
+
+  # Don't show the number of background jobs.
+  typeset -g POWERLEVEL9K_BACKGROUND_JOBS_VERBOSE=false
+  # Icon to show when there are background jobs.
+  typeset -g POWERLEVEL9K_BACKGROUND_JOBS_VISUAL_IDENTIFIER_EXPANSION='⇶'
+  # Background jobs icon color.
+  typeset -g POWERLEVEL9K_BACKGROUND_JOBS_VISUAL_IDENTIFIER_COLOR=2
+
+  # Context format: user@host.
+  typeset -g POWERLEVEL9K_CONTEXT_TEMPLATE='%n@%m'
+  # Default context color.
+  typeset -g POWERLEVEL9K_CONTEXT_FOREGROUND=244
+  # Context color when running with privileges.
+  typeset -g POWERLEVEL9K_CONTEXT_ROOT_FOREGROUND=11
+  # Don't show context unless running with privileges on in SSH.
+  typeset -g POWERLEVEL9K_CONTEXT_{DEFAULT,SUDO}_CONTENT_EXPANSION=
+  typeset -g POWERLEVEL9K_ALWAYS_SHOW_CONTEXT=true
+
+  # Python virtual environment color.
+  typeset -g POWERLEVEL9K_VIRTUALENV_FOREGROUND=6
+  # Show Python version next to the virtual environment name.
+  typeset -g POWERLEVEL9K_VIRTUALENV_SHOW_PYTHON_VERSION=true
+  typeset -g POWERLEVEL9K_VIRTUALENV_{LEFT,RIGHT}_DELIMITER=
+
+  typeset -g POWERLEVEL9K_ANACONDA_FOREGROUND=6
+  typeset -g POWERLEVEL9K_ANACONDA_SHOW_PYTHON_VERSION=true
+  typeset -g POWERLEVEL9K_ANACONDA_{LEFT,RIGHT}_DELIMITER=
+
+  typeset -g POWERLEVEL9K_PYENV_BACKGROUND=none
+  typeset -g POWERLEVEL9K_PYENV_FOREGROUND=6
+  typeset -g POWERLEVEL9K_PYENV_PROMPT_ALWAYS_SHOW=false
+
+  # Don't show trailing "/default" in kubernetes context.
+  typeset -g POWERLEVEL9K_KUBECONTEXT_SHOW_DEFAULT_NAMESPACE=false
+  # Kubernetes context classes for the purpose of using different colors with different contexts.
+  #
+  # POWERLEVEL9K_KUBECONTEXT_CLASSES is an array with even number of elements. The first element in
+  # each pair defines a pattern against which the current kubernetes context (in the format it is
+  # displayed in the prompt) gets matched. The second element defines the context class. Patterns
+  # are tried in order. The first match wins.
+  #
+  # For example, if your current kubernetes context is "deathray-testing", its class is TEST because
+  # "deathray-testing" doesn't match the pattern '*prod*' but does match '*test*'. Hence it'll be
+  # shown with the color of $POWERLEVEL9K_KUBECONTEXT_TEST_FOREGROUND.
+  typeset -g POWERLEVEL9K_KUBECONTEXT_CLASSES=(
+      '*prod*'  PROD
+      '*test*'  TEST
+      '*'       DEFAULT)
+  typeset -g POWERLEVEL9K_KUBECONTEXT_PROD_FOREGROUND=1
+  typeset -g POWERLEVEL9K_KUBECONTEXT_TEST_FOREGROUND=2
+  typeset -g POWERLEVEL9K_KUBECONTEXT_DEFAULT_FOREGROUND=3
+
+  typeset -g POWERLEVEL9K_TIME_FOREGROUND=66
+  # Format for the time segment: 09:51:02. See `man 3 strftime`.
+  typeset -g POWERLEVEL9K_TIME_FORMAT='%D{%H:%M:%S}'
+}
+
+(( ! p9k_lean_restore_aliases )) || setopt aliases
+'builtin' 'unset' 'p9k_lean_restore_aliases'
