e6d0da1c2e1b95e2a0169b7abcd6ef8b7650b4bf
romkatv <roman.perepelitsa@gmail.com>
2019-08-16 09:54:32 +0200

    remove POWERLEVEL9K_ALWAYS_SHOW_CONTEXT from configs as it confuses users

--- config/p10k-classic.zsh
+++ config/p10k-classic.zsh
@@ -431,7 +431,6 @@ fi
 
   # Don't show context unless running with privileges or in SSH.
   typeset -g POWERLEVEL9K_CONTEXT_{DEFAULT,SUDO}_{CONTENT,VISUAL_IDENTIFIER}_EXPANSION=
-  typeset -g POWERLEVEL9K_ALWAYS_SHOW_CONTEXT=true
 
   # Custom icon.
   # typeset -g POWERLEVEL9K_CONTEXT_VISUAL_IDENTIFIER_EXPANSION='⭐'
