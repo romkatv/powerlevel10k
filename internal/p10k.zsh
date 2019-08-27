e4d78d2a722c3d81a48d6eab4edc858c28fbf35f
romkatv <roman.perepelitsa@gmail.com>
2019-08-27 08:53:28 +0200

    fix custom prompt when the command is just one word; fixes #189

--- internal/p10k.zsh
+++ internal/p10k.zsh
@@ -1142,7 +1142,7 @@ _p9k_custom_prompt() {
   local segment_name=${1:u}
   local command=POWERLEVEL9K_CUSTOM_${segment_name}
   command=${(P)command}
-  local cmd="${(Q)${(z)command}[1]}"
+  local cmd="${(Q)${(Az)command}[1]}"
   (( $+functions[$cmd] || $+commands[$cmd] )) || return
   local content="$(eval $command)"
   [[ -n $content ]] || return
