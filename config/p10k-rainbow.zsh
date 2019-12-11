724dc171d3201de74eb9d961014cb21cfa3c8b9e
Adam Stroud <adam.stroud@gmail.com>
2019-12-10 20:14:45 -0500

    Fix typo

--- config/p10k-rainbow.zsh
+++ config/p10k-rainbow.zsh
@@ -327,7 +327,7 @@
   # typeset -g POWERLEVEL9K_DIR_CLASSES=()
 
   #####################################[ vcs: git status ]######################################
-  # Versio control system colors.
+  # Version control system colors.
   # typeset -g POWERLEVEL9K_VCS_CLEAN_BACKGROUND=2
   # typeset -g POWERLEVEL9K_VCS_MODIFIED_BACKGROUND=3
   # typeset -g POWERLEVEL9K_VCS_UNTRACKED_BACKGROUND=2
