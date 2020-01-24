261b19c6418cd47e78e438f340a6dda5dc55542c
Max Rydahl Andersen <max@xam.dk>
2020-01-24 22:39:23 +0100

    Add `oc` openshift to kubernetes context

--- config/p10k-classic.zsh
+++ config/p10k-classic.zsh
@@ -808,7 +808,7 @@
   #############[ kubecontext: current kubernetes context (https://kubernetes.io/) ]#############
   # Show kubecontext only when the the command you are typing invokes one of these tools.
   # Tip: Remove the next line to always show kubecontext.
-  typeset -g POWERLEVEL9K_KUBECONTEXT_SHOW_ON_COMMAND='kubectl|helm|kubens|kubectx'
+  typeset -g POWERLEVEL9K_KUBECONTEXT_SHOW_ON_COMMAND='kubectl|helm|kubens|kubectx|oc'
 
   # Kubernetes context classes for the purpose of using different colors, icons and expansions with
   # different contexts.
