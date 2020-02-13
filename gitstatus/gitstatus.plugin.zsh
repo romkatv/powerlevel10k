63183dea4bc3870a5414d542b2efb2467b0399fa
romkatv <roman.perepelitsa@gmail.com>
2020-02-13 21:33:01 +0100

    pull upstream changes from gitstatus

--- gitstatus/gitstatus.plugin.zsh
+++ gitstatus/gitstatus.plugin.zsh
@@ -1,19 +1,4 @@
-# Copyright 2019 Roman Perepelitsa.
-#
-# This file is part of GitStatus. It provides ZSH bindings.
-#
-# GitStatus is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# GitStatus is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with GitStatus. If not, see <https://www.gnu.org/licenses/>.
+# Zsh bindings for gitstatus.
 #
 # ------------------------------------------------------------------
 #
