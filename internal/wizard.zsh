9017e5815cb2b6a0c238d1386620199c49b458a0
brettm12345 <brettmandler@gmail.com>
2020-02-15 20:10:18 -0500

    Remove the extra variable and inline the test

--- internal/wizard.zsh
+++ internal/wizard.zsh
@@ -1506,7 +1506,7 @@ function ask_zshrc_edit() {
       r) return 1;;
       n) return 0;;
       y)
-        [[ $__p9k_writeable_zsh ]] || echo "Zshrc is readonly ignoring..."; return
+        [[ ! -e $__p9k_zshrc || -w $__p9k_zshrc ]] || echo "Zshrc is readonly ignoring..."; return
         write_zshrc=1
         if [[ -n $zshrc_content ]]; then
           zshrc_backup="$(mktemp ${TMPDIR:-/tmp}/.zshrc.XXXXXXXXXX)" || quit -c
