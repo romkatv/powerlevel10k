48a61471a7f257787332714b2f6a16e9ec04174d
romkatv <roman.perepelitsa@gmail.com>
2020-03-16 09:14:57 +0100

    parser bug fix: quote array elements after parameter expansion

--- internal/parser.zsh
+++ internal/parser.zsh
@@ -296,7 +296,7 @@ function _p9k_parse_buffer() {
         if [[ $token == $~var ]]; then
           n=${${token##[^[:IDENT:]]}%%[^[:IDENT:]]}
           [[ $token == *'"' ]] && v=("${(P)n}") || v=(${(P)n})
-          tokens[1,0]=(${(qq)v})
+          tokens[1,0]=(${(@qq)v})
           continue
         fi
       fi
