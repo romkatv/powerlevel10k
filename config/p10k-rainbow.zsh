6c83ace41ce41a362b68a58be45ce29164da8fa5
romkatv <roman.perepelitsa@gmail.com>
2020-05-17 08:27:28 +0200

    uncomment POWERLEVEL9K_DIR_BACKGROUND

--- config/p10k-rainbow.zsh
+++ config/p10k-rainbow.zsh
@@ -201,7 +201,7 @@
 
   ##################################[ dir: current directory ]##################################
   # Current directory background color.
-  # typeset -g POWERLEVEL9K_DIR_BACKGROUND=4
+  typeset -g POWERLEVEL9K_DIR_BACKGROUND=4
   # Default current directory foreground color.
   typeset -g POWERLEVEL9K_DIR_FOREGROUND=254
   # If directory is too long, shorten some of its segments to the shortest possible unique
