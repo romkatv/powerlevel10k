f96c1eb5e591bc10fd2e4e5f0bcd016ab0aba809
romkatv <roman.perepelitsa@gmail.com>
2020-05-18 07:00:11 +0200

    add POWERLEVEL9K_VIRTUALENV_SHOW_WITH_PYENV=false to config templates
    See #730.

--- config/p10k-rainbow.zsh
+++ config/p10k-rainbow.zsh
@@ -881,6 +881,8 @@
   # typeset -g POWERLEVEL9K_VIRTUALENV_BACKGROUND=4
   # Don't show Python version next to the virtual environment name.
   typeset -g POWERLEVEL9K_VIRTUALENV_SHOW_PYTHON_VERSION=false
+  # Don't show virtualenv if pyenv is already shown.
+  typeset -g POWERLEVEL9K_VIRTUALENV_SHOW_WITH_PYENV=false
   # Separate environment name from Python version only with a space.
   typeset -g POWERLEVEL9K_VIRTUALENV_{LEFT,RIGHT}_DELIMITER=
   # Custom icon.
