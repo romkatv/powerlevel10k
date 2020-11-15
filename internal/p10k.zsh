romkatv/powerlevel10k

commit 8488f7c75c34b7cd1d0f2868dc674197b74aede3

set P9K_PROMPT=transient for the duration of zle-line-finish (#1105)

internal/p10k.zsh
@@ -3934,7 +3934,8 @@ function _p9k_vcs_resume() {
 }
 
 function _p9k_vcs_gitstatus() {
-  if [[ $_p9k__refresh_reason == precmd ]]; then
+  if [[ $_p9k__refresh_reason == precmd ]] && (( !_p9k__vcs_called )); then
+    typeset -gi _p9k__vcs_called=1
     if (( $+_p9k__gitstatus_next_dir )); then
       _p9k__gitstatus_next_dir=$_p9k__cwd_a
     else
@@ -5597,6 +5598,8 @@ _p9k_prompt_net_iface_sync() {
 }
 
 function _p9k_set_prompt() {
+  local -i _p9k__vcs_called
+
   PROMPT=
   RPROMPT=
   [[ $1 == instant_ ]] || PROMPT+='${$((_p9k_on_expand()))+}'
@@ -7367,6 +7370,8 @@ function _p9k_on_widget_zle-line-init() {
 
 function _p9k_on_widget_zle-line-finish() {
   (( $+_p9k__line_finished )) && return
+
+  local P9K_PROMPT=transient
 
   _p9k__line_finished=
   (( _p9k_reset_on_line_finish )) && __p9k_reset_state=2

commit 09be56bb532d61420c6eb2292874fcd00246a15f

fix a bug introduced in 8488f7c (#1107)

internal/p10k.zsh
@@ -6634,6 +6634,7 @@ _p9k_precmd_impl() {
       local -F start_time=EPOCHREALTIME
       unset _p9k__vcs
       unset _p9k__vcs_timeout
+      local -i _p9k__vcs_called
       _p9k_vcs_gitstatus
       local -i fast_vcs=1
     fi