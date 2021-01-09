33f20f5eaea0de5f08d77869f8cc087ddf798351
Julio Carlos Barrera Juez <juliocarlos@gmail.com>
2021-01-09 23:49:12 +0100

    Add tailscale in VPN network interface regex. (#1208)

--- config/p10k-lean-8colors.zsh
+++ config/p10k-lean-8colors.zsh
@@ -1419,7 +1419,7 @@
   typeset -g POWERLEVEL9K_VPN_IP_CONTENT_EXPANSION=
   # Regular expression for the VPN network interface. Run `ifconfig` or `ip -4 a show` while on VPN
   # to see the name of the interface.
-  typeset -g POWERLEVEL9K_VPN_IP_INTERFACE='(gpd|wg|(.*tun))[0-9]*'
+  typeset -g POWERLEVEL9K_VPN_IP_INTERFACE='(gpd|wg|(.*tun)|tailscale)[0-9]*'
   # If set to true, show one segment per matching network interface. If set to false, show only
   # one segment corresponding to the first matching network interface.
   # Tip: If you set it to true, you'll probably want to unset POWERLEVEL9K_VPN_IP_CONTENT_EXPANSION.
