be3724bc806a2dd7fbcb281a153b11ab19d8923d
Roman Perepelitsa <roman.perepelitsa@gmail.com>
2022-07-11 10:08:06 +0200

    typo in comments

--- config/p10k-rainbow.zsh
+++ config/p10k-rainbow.zsh
@@ -344,7 +344,7 @@
   # typeset -g POWERLEVEL9K_DIR_PREFIX='in '
 
   #####################################[ vcs: git status ]######################################
-  # Version control system colors.
+  # Version control background colors.
   typeset -g POWERLEVEL9K_VCS_CLEAN_BACKGROUND=2
   typeset -g POWERLEVEL9K_VCS_MODIFIED_BACKGROUND=3
   typeset -g POWERLEVEL9K_VCS_UNTRACKED_BACKGROUND=2
