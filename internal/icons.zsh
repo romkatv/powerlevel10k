f02b8d365baae5336a5399256739930fa323f3f1
Roman Perepelitsa <roman.perepelitsa@gmail.com>
2023-04-02 13:14:47 +0200

    add POWERLEVEL9K_MODE=nerdfont-v3 (#2217)
    This is Step 1 of https://github.com/romkatv/powerlevel10k/issues/2217#issuecomment-1493271666.

--- internal/icons.zsh
+++ internal/icons.zsh
@@ -423,6 +423,141 @@ function _p9k_init_icons() {
         ARCH_ICON                      'arch'
       )
     ;;
+    'nerdfont-v3')
+      # In this version of Nerd Fonts the Material icons are mapped to U+F0001-U+F19C3.
+      # The font may also have Material icons in the old range of U+F500-U+FD46 but
+      # powerlevel10k won't rely on them.
+      icons=(
+        RULER_CHAR                     '\u2500'               # ─
+        LEFT_SEGMENT_SEPARATOR         '\uE0B0'               # 
+        RIGHT_SEGMENT_SEPARATOR        '\uE0B2'               # 
+        LEFT_SEGMENT_END_SEPARATOR     ' '                    #
+        LEFT_SUBSEGMENT_SEPARATOR      '\uE0B1'               # 
+        RIGHT_SUBSEGMENT_SEPARATOR     '\uE0B3'               # 
+        CARRIAGE_RETURN_ICON           '\u21B5'               # ↵
+        ROOT_ICON                      '\uE614'$q             # 
+        SUDO_ICON                      '\uF09C'$s             # 
+        RUBY_ICON                      '\uF219 '              # 
+        AWS_ICON                       '\uF270'$s             # 
+        AWS_EB_ICON                    '\UF1BD'$q$q           # 
+        BACKGROUND_JOBS_ICON           '\uF013 '              # 
+        TEST_ICON                      '\uF188'$s             # 
+        TODO_ICON                      '\u2611'               # ☑
+        BATTERY_ICON                   '\UF240 '              # 
+        DISK_ICON                      '\uF0A0'$s             # 
+        OK_ICON                        '\uF00C'$s             # 
+        FAIL_ICON                      '\uF00D'               # 
+        SYMFONY_ICON                   '\uE757'               # 
+        NODE_ICON                      '\uE617 '              # 
+        NODEJS_ICON                    '\uE617 '              # 
+        MULTILINE_FIRST_PROMPT_PREFIX  '\u256D\U2500'         # ╭─
+        MULTILINE_NEWLINE_PROMPT_PREFIX '\u251C\U2500'        # ├─
+        MULTILINE_LAST_PROMPT_PREFIX   '\u2570\U2500 '        # ╰─
+        APPLE_ICON                     '\uF179'               # 
+        WINDOWS_ICON                   '\uF17A'$s             # 
+        FREEBSD_ICON                   '\UF30C '              # 
+        ANDROID_ICON                   '\uF17B'               # 
+        LINUX_ARCH_ICON                '\uF303'               # 
+        LINUX_CENTOS_ICON              '\uF304'$s             # 
+        LINUX_COREOS_ICON              '\uF305'$s             # 
+        LINUX_DEBIAN_ICON              '\uF306'               # 
+        LINUX_RASPBIAN_ICON            '\uF315'               # 
+        LINUX_ELEMENTARY_ICON          '\uF309'$s             # 
+        LINUX_FEDORA_ICON              '\uF30a'$s             # 
+        LINUX_GENTOO_ICON              '\uF30d'$s             # 
+        LINUX_MAGEIA_ICON              '\uF310'               # 
+        LINUX_MINT_ICON                '\uF30e'$s             # 
+        LINUX_NIXOS_ICON               '\uF313'$s             # 
+        LINUX_MANJARO_ICON             '\uF312'$s             # 
+        LINUX_DEVUAN_ICON              '\uF307'$s             # 
+        LINUX_ALPINE_ICON              '\uF300'$s             # 
+        LINUX_AOSC_ICON                '\uF301'$s             # 
+        LINUX_OPENSUSE_ICON            '\uF314'$s             # 
+        LINUX_SABAYON_ICON             '\uF317'$s             # 
+        LINUX_SLACKWARE_ICON           '\uF319'$s             # 
+        LINUX_VOID_ICON                '\uF17C'               # 
+        LINUX_ARTIX_ICON               '\uF17C'               # 
+        LINUX_UBUNTU_ICON              '\uF31b'$s             # 
+        LINUX_RHEL_ICON                '\uF316'$s             # 
+        LINUX_AMZN_ICON                '\uF270'$s             # 
+        LINUX_ICON                     '\uF17C'               # 
+        SUNOS_ICON                     '\uF185 '              # 
+        HOME_ICON                      '\uF015'$s             # 
+        HOME_SUB_ICON                  '\uF07C'$s             # 
+        FOLDER_ICON                    '\uF115'$s             # 
+        ETC_ICON                       '\uF013'$s             # 
+        NETWORK_ICON                   '\UF0378'$s            # 󰍸
+        LOAD_ICON                      '\uF080 '              # 
+        SWAP_ICON                      '\uF464'$s             # 
+        RAM_ICON                       '\uF0E4'$s             # 
+        SERVER_ICON                    '\uF0AE'$s             # 
+        VCS_UNTRACKED_ICON             '\uF059'$s             # 
+        VCS_UNSTAGED_ICON              '\uF06A'$s             # 
+        VCS_STAGED_ICON                '\uF055'$s             # 
+        VCS_STASH_ICON                 '\uF01C '              # 
+        VCS_INCOMING_CHANGES_ICON      '\uF01A '              # 
+        VCS_OUTGOING_CHANGES_ICON      '\uF01B '              # 
+        VCS_TAG_ICON                   '\uF02B '              # 
+        VCS_BOOKMARK_ICON              '\uF461 '              # 
+        VCS_COMMIT_ICON                '\uE729 '              # 
+        VCS_BRANCH_ICON                '\uF126 '              # 
+        VCS_REMOTE_BRANCH_ICON         '\uE728 '              # 
+        VCS_LOADING_ICON               ''                     #
+        VCS_GIT_ICON                   '\uF1D3 '              # 
+        VCS_GIT_GITHUB_ICON            '\uF113 '              # 
+        VCS_GIT_BITBUCKET_ICON         '\uE703 '              # 
+        VCS_GIT_GITLAB_ICON            '\uF296 '              # 
+        VCS_HG_ICON                    '\uF0C3 '              # 
+        VCS_SVN_ICON                   '\uE72D'$q             # 
+        RUST_ICON                      '\uE7A8'$q             # 
+        PYTHON_ICON                    '\UE73C '              # 
+        SWIFT_ICON                     '\uE755'               # 
+        GO_ICON                        '\uE626'               # 
+        GOLANG_ICON                    '\uE626'               # 
+        PUBLIC_IP_ICON                 '\UF0AC'$s             # 
+        LOCK_ICON                      '\UF023'               # 
+        NORDVPN_ICON                   '\UF023'               # 
+        EXECUTION_TIME_ICON            '\uF252'$s             # 
+        SSH_ICON                       '\uF489'$s             # 
+        VPN_ICON                       '\UF023'               # 
+        KUBERNETES_ICON                '\U2388'               # ⎈
+        DROPBOX_ICON                   '\UF16B'$s             # 
+        DATE_ICON                      '\uF073 '              # 
+        TIME_ICON                      '\uF017 '              # 
+        JAVA_ICON                      '\uE738'               # 
+        LARAVEL_ICON                   '\ue73f'$q             # 
+        RANGER_ICON                    '\uF00b '              # 
+        MIDNIGHT_COMMANDER_ICON        'mc'                   # mc
+        VIM_ICON                       '\uE62B'               # 
+        TERRAFORM_ICON                 '\uF1BB '              # 
+        PROXY_ICON                     '\u2194'               # ↔
+        DOTNET_ICON                    '\uE77F'               # 
+        DOTNET_CORE_ICON               '\uE77F'               # 
+        AZURE_ICON                     '\UF0805'              # 󰠅
+        DIRENV_ICON                    '\u25BC'               # ▼
+        FLUTTER_ICON                   'F'                    # F
+        GCLOUD_ICON                    '\UF02AD'              # 󰊭
+        LUA_ICON                       '\uE620'               # 
+        PERL_ICON                      '\uE769'               # 
+        NNN_ICON                       'nnn'                  # nnn
+        LF_ICON                        'lf'                   # lf
+        XPLR_ICON                      'xplr'                 # xplr
+        TIMEWARRIOR_ICON               '\uF49B'               # 
+        TASKWARRIOR_ICON               '\uF4A0 '              # 
+        NIX_SHELL_ICON                 '\uF313 '              # 
+        WIFI_ICON                      '\uF1EB '              # 
+        ERLANG_ICON                    '\uE7B1 '              # 
+        ELIXIR_ICON                    '\uE62D'               # 
+        POSTGRES_ICON                  '\uE76E'               # 
+        PHP_ICON                       '\uE608'               # 
+        HASKELL_ICON                   '\uE61F'               # 
+        PACKAGE_ICON                   '\UF03D7'              # 󰏗
+        JULIA_ICON                     '\uE624'               # 
+        SCALA_ICON                     '\uE737'               # 
+        TOOLBOX_ICON                   '\uE20F'$s             # 
+        ARCH_ICON                      '\uE266'               # 
+      )
+    ;;
     'nerdfont-complete'|'nerdfont-fontconfig')
       # nerd-font patched (complete) font required! See
       # https://github.com/ryanoasis/nerd-fonts
