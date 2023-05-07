4ed8aae3246da5c8f16978266ba6af139a1d06ca
Zaidhaan Hussain <zaidhaan03@gmail.com>
2023-05-07 17:10:08 +0800

    nvm: implement POWERLEVEL9K_NVM_PROMPT_ALWAYS_SHOW and default to false

--- config/p10k-classic.zsh
+++ config/p10k-classic.zsh
@@ -1007,6 +1007,9 @@
   ##############[ nvm: node.js version from nvm (https://github.com/nvm-sh/nvm) ]###############
   # Nvm color.
   typeset -g POWERLEVEL9K_NVM_FOREGROUND=70
+  # If set to false, hide node version if it's the same as default:
+  # $(nvm version current) == $(nvm version default).
+  typeset -g POWERLEVEL9K_NVM_PROMPT_ALWAYS_SHOW=false
   # If set to false, hide node version if it's equal to "system".
   typeset -g POWERLEVEL9K_NVM_SHOW_SYSTEM=true
   # Custom icon.
--- config/p10k-rainbow.zsh
+++ config/p10k-rainbow.zsh
@@ -1053,6 +1053,9 @@
   # Nvm color.
   typeset -g POWERLEVEL9K_NVM_FOREGROUND=0
   typeset -g POWERLEVEL9K_NVM_BACKGROUND=5
+  # If set to false, hide node version if it's the same as default:
+  # $(nvm version current) == $(nvm version default).
+  typeset -g POWERLEVEL9K_NVM_PROMPT_ALWAYS_SHOW=false
   # If set to false, hide node version if it's equal to "system".
   typeset -g POWERLEVEL9K_NVM_SHOW_SYSTEM=true
   # Custom icon.
