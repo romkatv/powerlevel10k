cc6ed4be416b70fe4e3f97d17061c751abaca04f
Roman Perepelitsa <roman.perepelitsa@gmail.com>
2023-12-16 08:36:29 +0100

    add `cdk` to POWERLEVEL9K_AWS_SHOW_ON_COMMAND (#1104)

--- config/p10k-rainbow.zsh
+++ config/p10k-rainbow.zsh
@@ -1448,7 +1448,7 @@
   #[ aws: aws profile (https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-profiles.html) ]#
   # Show aws only when the command you are typing invokes one of these tools.
   # Tip: Remove the next line to always show aws.
-  typeset -g POWERLEVEL9K_AWS_SHOW_ON_COMMAND='aws|awless|terraform|pulumi|terragrunt'
+  typeset -g POWERLEVEL9K_AWS_SHOW_ON_COMMAND='aws|awless|cdk|terraform|pulumi|terragrunt'
 
   # POWERLEVEL9K_AWS_CLASSES is an array with even number of elements. The first element
   # in each pair defines a pattern against which the current AWS profile gets matched.
