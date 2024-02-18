bb16e366c3f7d68f73fa9129f3593524ff6420ac
Adam Schumacher <adam@genericor.com>
2024-02-17 22:17:50 -0500

    Undelete ubuntu. I shouldn't code this tired.

--- internal/p10k.zsh
+++ internal/p10k.zsh
@@ -8633,6 +8633,7 @@ function _p9k_init_cacheable() {
               _p9k_set_os Linux LINUX_DEBIAN_ICON
             fi
             ;;          
+          *ubuntu*)                _p9k_set_os Linux LINUX_UBUNTU_ICON;;
           *elementary*)            _p9k_set_os Linux LINUX_ELEMENTARY_ICON;;
           *fedora*)                _p9k_set_os Linux LINUX_FEDORA_ICON;;
           *coreos*)                _p9k_set_os Linux LINUX_COREOS_ICON;;
